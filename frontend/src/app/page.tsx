0e5832094bf71848a0ea90738f5844bd3219eec2
Twathik <takdemt.wathik@gmail.com>
2024-09-21 00:48:53 +0100

    fix bad limk in index

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -12,19 +12,21 @@ export default function Home() {
   return (
     <main>
       <div className="flex flex-row justify-center">
-        <Card>
+        <Card className="mt-40 h-[20vh]">
           <CardHeader>
             <CardTitle>Exemples de graphiques</CardTitle>
             <CardDescription>
               Veuillez selectionnez un type de graphique
             </CardDescription>
           </CardHeader>
-          <CardContent>
-            <div className="flex justify-center items-center gap-4">
-              <Link href="/categoryChart" prefetch>
-                <Button>Category Charts</Button>
+          <CardContent className="flex justify-center item-center gap-4 ">
+            <div className="gap-4 flex">
+              <Link href="/barChart" prefetch>
+                <Button>Bar Charts</Button>
               </Link>
-
+              <Link href="/pieChart" prefetch>
+                <Button>Pie Chart</Button>
+              </Link>
               <Link href="/timeSeriesChart" prefetch>
                 <Button>Time Series Chart</Button>
               </Link>
